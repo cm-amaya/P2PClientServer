86655a20f78429832226adf6a23d7935e2d969d7
LAPTOP-C6DCBCLI\cmama <cm.amaya@hotmail.com>
2018-10-07 14:14:06 -0500

    Cliente y Tracker P2P

--- src/main/java/com/uniandes/labredes/Cliente.java
+++ src/main/java/com/uniandes/labredes/Cliente.java
@@ -4,13 +4,25 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.nio.channels.UnsupportedAddressTypeException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Enumeration;
 import java.util.Observable;
 import java.util.Observer;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.PatternLayout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,73 +31,152 @@ import com.turn.ttorrent.client.SharedTorrent;
 
 @SuppressWarnings("deprecation")
 public class Cliente {
+	
+	/**
+	 * Bytes en un MegaByte
+	 */
+	public final static double MB = Math.pow(1024,2);
 
 	private static final Logger logger =
 			LoggerFactory.getLogger(Cliente.class);
+
+	private static String start_date;
+	private static String fileName;
+	private static String outputDir;
+	static boolean finish = false;
+	
+	private static Inet4Address getIPv4Address(String iface)
+			throws SocketException, UnsupportedAddressTypeException,
+			UnknownHostException {
+			if (iface != null) {
+				Enumeration<InetAddress> addresses =
+					NetworkInterface.getByName(iface).getInetAddresses();
+				while (addresses.hasMoreElements()) {
+					InetAddress addr = addresses.nextElement();
+					if (addr instanceof Inet4Address) {
+						return (Inet4Address)addr;
+					}
+				}
+			}
+
+			InetAddress localhost = InetAddress.getLocalHost();
+			if (localhost instanceof Inet4Address) {
+				return (Inet4Address)localhost;
+			}
+
+			throw new UnsupportedAddressTypeException();
+		}
+
+	private static void startLog() {
+		String logName = "log-"+fileName+"_"+start_date+".txt";
+		File logPath = new File(outputDir,logName);
+		try {
+			final BufferedWriter writer = new BufferedWriter(new FileWriter(logPath.getPath()));
+			writer.write("Archivo solicitado: "+fileName);
+			writer.newLine();
+			writer.write("Comienzo proceso: "+start_date);
+			writer.newLine();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void appendToLog(String line) {
+
+		try {
+			String logName = "log-"+fileName+"_"+start_date+".txt";
+			File logPath = new File(outputDir,logName);
+			BufferedWriter bw = new BufferedWriter( new FileWriter(logPath.getPath(), true));
+			bw.write(line + System.getProperty("line.separator"));
+			bw.close();
+			logger.info(line);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static String roundTwo(double number) {
+		 return Double.toString((double) Math.round(number * 100) / 100);
+	}
 
 	/**
 	 * Default data output directory.
 	 */
 	private static final String DEFAULT_OUTPUT_DIRECTORY = "/tmp";
 
-	@SuppressWarnings("deprecation")
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws UnknownHostException, UnsupportedAddressTypeException, SocketException {
+
+		BasicConfigurator.configure(new ConsoleAppender(new PatternLayout("%d [%-25t] %-5p: %m%n")));
+
+		Inet4Address address = getIPv4Address(args[0]);
+		
+		System.out.println("Direccion: "+address.toString());
+
+		File torrent = new File(args[1]);
+		
+		System.out.println("Torrent: "+torrent.getAbsolutePath());
+
+		if(!torrent.isFile()) {
+			System.out.println("No existe el archivo torrent");
+			return;
+		} else if(!torrent.getName().contains(".torrent")) {
+			System.out.println("Ingrese torrent valido");
+			return;
+		}
+
+		File outputDirFile = new File(args[2]);
+
+		if(!outputDirFile.exists()) {
+			outputDirFile = new File(DEFAULT_OUTPUT_DIRECTORY);
+			if(!outputDirFile.exists()) {
+				outputDirFile.mkdir();
+			}
+		}
 		try {
 
-			//InetAddress address = InetAddress.getLocalHost();
-			InetAddress address = new InetSocketAddress(0).getAddress();
+			Client c = new Client(address,SharedTorrent.fromFile(torrent,outputDirFile));
 			
-			File torrent = new File(args[0]);
+			c.setMaxUploadRate(0.0);
+			c.setMaxDownloadRate(0.0);
 
-			Client c = new Client(address,SharedTorrent.fromFile(torrent,new File(args[1])));
-
-			String fileName =torrent.getName();
+			fileName =torrent.getName();
 			if (fileName.indexOf(".") > 0) {
 				fileName = fileName.substring(0, fileName.lastIndexOf("."));
 			}
-			String start_date = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date());
-			String logName = "log-"+fileName+"_"+start_date+".txt";
-			final BufferedWriter writer = new BufferedWriter(new FileWriter(logName));
+			start_date = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date());
+			
+			outputDir= outputDirFile.getAbsolutePath();
 
-			writer.write("Archivo solicitado: "+fileName);
-			writer.newLine();
-			writer.write("Comienzo proceso: "+start_date);
-			writer.newLine();
+			startLog();
 
 			final long startTime = System.currentTimeMillis();
-			final int chunk = 1;
 
 			c.addObserver(new Observer() {
 				public void update(Observable observable, Object data) {
 					Client client = (Client) observable;
-					float progress = client.getTorrent().getCompletion();
-					System.out.println(progress);
 					if(client.getTorrent().isInitialized()) {
-						int current_pieces = client.getTorrent().getCompletedPieces().length();
+						int current_pieces = client.getTorrent().getCompletedPieces().cardinality();
+						int asking_pieces = client.getTorrent().getRequestedPieces().cardinality();
+						double bytes_complete =  client.getTorrent().getDownloaded()/MB;
+						double bytes_left = client.getTorrent().getLeft()/MB; 
 						int peers = client.getPeers().size();	
 						long currentTime = System.currentTimeMillis();
 						long elapsedTime = currentTime - startTime;
-						try {
-							writer.write("Peers: "+peers+" - Chunks completados: "+current_pieces+" - Tiempo transmision: "+(elapsedTime/1000)+" segundos");
-							writer.newLine();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
+						appendToLog("Peers: "+peers+" - Chunks completados: "+current_pieces+" - MB descargados: "+roundTwo(bytes_complete)+" - Tiempo transmision: "+(elapsedTime/1000)+" segundos");
 
-						if(client.getTorrent().isComplete()) {
-							try {
-								writer.write("Termino proceso - Chunks completados: "+current_pieces+" - Tiempo transmision: "+(elapsedTime/1000)+" segundos");
-								writer.close();
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
+						if(client.getTorrent().isComplete() && !finish) {
+							appendToLog("Termino proceso - Chunks completados: "+current_pieces+" - Tiempo transmision: "+(elapsedTime/1000)+" segundos");
+							finish = true;
 						}
 					}
 				}
 			});
 
 
-			c.share(3600);
+			c.share(1200);
 
 		} catch (Exception e) {
 			logger.error("Fatal error: {}", e.getMessage(), e);
--- src/main/java/com/uniandes/labredes/ServidorTracker.java
+++ src/main/java/com/uniandes/labredes/ServidorTracker.java
@@ -3,8 +3,12 @@ package com.uniandes.labredes;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.PatternLayout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,6 +24,8 @@ public class ServidorTracker {
 
 	public static void main(String[] args) {
 		
+		BasicConfigurator.configure(new ConsoleAppender(new PatternLayout("%d [%-25t] %-5p: %m%n")));
+		
 		FilenameFilter filter = new FilenameFilter() {
 			public boolean accept(File dir, String name) {
 			    return name.endsWith(".torrent");
@@ -27,9 +33,9 @@ public class ServidorTracker {
 		};
 		
 		try {
-			Tracker tracker = new Tracker(new InetSocketAddress(6969));
+			Tracker tracker = new Tracker(new InetSocketAddress(8080));
 			
-			for (File f : new File("/path/to/torrent/files").listFiles(filter)) {
+			for (File f : new File(".").listFiles(filter)) {
 				  tracker.announce(TrackedTorrent.load(f));
 			}
 			
